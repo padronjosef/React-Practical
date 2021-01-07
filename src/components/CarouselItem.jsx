21f8c76cfd502fe4f919ecdeb6237a62de734c11
Jose Padron <josepadron.go@gmail.com>
2021-01-06 23:09:07 -0500

    adding dependecy proptypes to the project and aplying it to CarouselItem.jsx

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import '../assets/styles/Components/CarouselItem.scss'
 
 import playIcon from '../assets/static/play-icon.png'
@@ -19,5 +21,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+}
 
 export default CarouselItem
